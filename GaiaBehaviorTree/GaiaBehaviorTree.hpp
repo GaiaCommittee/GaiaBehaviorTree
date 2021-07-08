df3edac1e148e3e6dc7ec3d643d892be06cbd8f4
Vincent <rumble.vecent@outlook.com>
2021-07-08 23:29:33 +0800

    Add containers and decorators into the index header.

--- GaiaBehaviorTree/GaiaBehaviorTree.hpp
+++ GaiaBehaviorTree/GaiaBehaviorTree.hpp
@@ -5,5 +5,18 @@
 #include "Decorator.hpp"
 #include "Context.hpp"
 
+#include "Containers/Sequence.hpp"
+#include "Containers/Select.hpp"
+#include "Containers/Random.hpp"
+#include "Containers/Branch.hpp"
+#include "Containers/ParallelSequence.hpp"
+#include "Containers/ParallelSelect.hpp"
+
+#include "Decorators/If.hpp"
+#include "Decorators/Reverse.hpp"
+#include "Decorators/Suppress.hpp"
+#include "Decorators/Until.hpp"
+#include "Decorators/While.hpp"
+
 namespace Gaia::BehaviorTree
 {}
